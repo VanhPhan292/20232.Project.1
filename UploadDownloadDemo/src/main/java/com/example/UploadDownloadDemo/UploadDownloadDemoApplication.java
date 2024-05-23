e03a7180c3e194688a5ad8719cb1461dabcfd08a
VanhPhan292 <phan.viet.anh.292@gmail.com>
2024-05-23 22:16:05 +0700

    demo for upload download feature

---
+++ UploadDownloadDemo/src/main/java/com/example/UploadDownloadDemo/UploadDownloadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.UploadDownloadDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDownloadDemoApplication.class, args);
+	}
+
+}
---
+++ UploadDownloadDemo/src/main/java/com/example/upload/FileUploadRespond.java
@@ -0,0 +1,25 @@
+package com.example.upload;
+
+public class FileUploadRespond {
+	private String fileName;
+    private String downloadUri;
+    private long size;
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getDownloadUri() {
+		return downloadUri;
+	}
+	public void setDownloadUri(String downloadUri) {
+		this.downloadUri = downloadUri;
+	}
+	public long getSize() {
+		return size;
+	}
+	public void setSize(long size) {
+		this.size = size;
+	}
+}
